2bd4c594c39fc731f8a67573994730f42a0c4d5c
Golang <root@g.com>
2024-05-07 17:04:23 +0000

    删除不需要的including

--- cmd/sing-box/cmd_check.go
+++ cmd/sing-box/cmd_check.go
@@ -3,27 +3,8 @@ package main
 import (
 	"context"
 
-	"github.com/sagernet/sing-box"
-	"github.com/sagernet/sing-box/log"
-
-	"github.com/spf13/cobra"
+	box "github.com/sagernet/sing-box"
 )
-
-var commandCheck = &cobra.Command{
-	Use:   "check",
-	Short: "Check configuration",
-	Run: func(cmd *cobra.Command, args []string) {
-		err := check()
-		if err != nil {
-			log.Fatal(err)
-		}
-	},
-	Args: cobra.NoArgs,
-}
-
-func init() {
-	mainCommand.AddCommand(commandCheck)
-}
 
 func check() error {
 	options, err := readConfigAndMerge()
