424db6d23bf6fc295f75432753b8c5e370688084
Golang <root@g.com>
2024-05-07 15:46:31 +0000

    删除Tuic

--- inbound/builder.go
+++ inbound/builder.go
@@ -42,8 +42,6 @@ func New(ctx context.Context, router adapter.Router, logger log.ContextLogger, o
 		return NewHysteria(ctx, router, logger, options.Tag, options.HysteriaOptions)
 	case C.TypeVLESS:
 		return NewVLESS(ctx, router, logger, options.Tag, options.VLESSOptions)
-	case C.TypeTUIC:
-		return NewTUIC(ctx, router, logger, options.Tag, options.TUICOptions)
 	case C.TypeHysteria2:
 		return NewHysteria2(ctx, router, logger, options.Tag, options.Hysteria2Options)
 	default:
